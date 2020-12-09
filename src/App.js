b1f3368687397fa3844d8fe976dd371f04400ca4
Sparsh Kesari <39815871+SparshKesari@users.noreply.github.com>
2020-12-09 21:51:34 +0530

    codeofconduct added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import LayoutDefault from './layouts/LayoutDefault';
 
 // Views 
 import Home from './views/Home';
+import Coc from './views/Coc';
 
 // Initialize Google Analytics
 ReactGA.initialize(process.env.REACT_APP_GA_CODE);
@@ -37,6 +38,8 @@ const App = () => {
       children={() => (
         <Switch>
           <AppRoute exact path="/" component={Home} layout={LayoutDefault} />
+          <AppRoute exact path="/CodeofConduct" component={Coc} layout={LayoutDefault} />
+
         </Switch>
       )} />
   );
